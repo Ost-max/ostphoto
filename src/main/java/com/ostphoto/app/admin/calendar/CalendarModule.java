b6fc5b1803210bead5d1889345fa4eb0e9417d16
Maxim Ostrovsky <ost-max@yndex.ru>
2013-09-15 20:11:35 +0400

    Adding calendar module file.

---
+++ src/main/java/com/ostphoto/app/admin/calendar/CalendarModule.java
@@ -0,0 +1,21 @@
+package com.ostphoto.app.admin.calendar;
+
+import java.util.Map;
+
+import com.ostphoto.app.admin.IModule;
+
+public class CalendarModule implements IModule {
+
+	@Override
+	public String getSmallViewName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Map<String, ?> getSmallAttributes() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
